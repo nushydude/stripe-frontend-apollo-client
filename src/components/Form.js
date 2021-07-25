2ff49367b6eae3fa25e351bd343a9051cbff95aa
Anusha Dharmasena <cdan.dharmasena@gmail.com>
2021-07-25 19:52:45 +1000

    small changes

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,9 @@
-import gql from 'graphql-tag';
-import * as React from 'react';
-import { Mutation } from 'react-apollo';
-import { CardElement, injectStripe } from 'react-stripe-elements';
-import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
-import InputLabel from '@material-ui/core/InputLabel';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
-
+import gql from "graphql-tag";
+import * as React from "react";
+import { Mutation } from "react-apollo";
+import { CardElement, injectStripe } from "react-stripe-elements";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 const CREATE_PURCHASE_MUTATION = /* GraphQL */ gql`
   mutation CreatePurchase($input: CreatePurchaseInput!) {
     result: createPurchase(input: $input) {
@@ -23,13 +18,24 @@ const CREATE_PURCHASE_MUTATION = /* GraphQL */ gql`
 `;
 
 class FormComponent extends React.Component {
-  state = { name: '', amount: 0, currency: 'aud' };
+  state = { name: "", amount: 0, currency: "aud" };
 
   render() {
+    console.log("state:", this.state);
+
     return (
       <Mutation mutation={CREATE_PURCHASE_MUTATION}>
-        {createPurchase => (
-          <React.Fragment>
+        {(createPurchase) => (
+          <div
+            style={{
+              margin: "0 auto",
+              padding: 20,
+              border: "1px solid #777",
+              maxWidth: 400,
+              borderRadius: 5,
+              background: "#eee"
+            }}
+          >
             <Typography variant="h6" gutterBottom>
               Create Purchase Form
             </Typography>
@@ -37,7 +43,7 @@ class FormComponent extends React.Component {
             <form
               noValidate
               autoComplete="off"
-              onSubmit={async e => {
+              onSubmit={async (e) => {
                 e.preventDefault();
                 const { amount, currency, name } = this.state;
 
@@ -45,79 +51,99 @@ class FormComponent extends React.Component {
 
                 try {
                   token = await this.props.stripe.createToken({
-                    name,
+                    name
                   });
                 } catch (error) {
-                  console.log('createToken error:', error.message);
+                  console.log("createToken error:", error.message);
 
                   return;
                 }
 
-                console.log('token:', token.token.id);
+                console.log("token:", token.token.id);
 
                 const variables = {
                   input: {
                     amount: parseFloat(amount),
                     currency,
-                    token: token.token.id,
-                  },
+                    token: token.token.id
+                  }
                 };
 
-                console.log('variables:', variables);
+                console.log("variables:", variables);
 
                 try {
                   const createPurchaseResult = await createPurchase({
-                    variables,
+                    variables
                   });
 
-                  console.log('createPurchaseResult:', createPurchaseResult);
+                  console.log("createPurchaseResult:", createPurchaseResult);
                 } catch (error) {
-                  console.log('createPurchase error:', error);
+                  console.log("createPurchase error:", error);
                 }
               }}
             >
-              <TextField
-                required
-                id="name"
-                name="name"
-                label="Name"
-                fullWidth
-                autoComplete="name"
-                onChange={e => this.setState({ name: e.target.value })}
-              />
+              <div style={{ marginBottom: 20 }}>
+                <label htmlFor="name">
+                  <div>Name</div>
 
-              <TextField
-                required
-                id="amount"
-                name="amount"
-                label="Amount"
-                fullWidth
-                autoComplete="amount"
-                onChange={e => this.setState({ amount: e.target.value })}
-              />
+                  <input
+                    style={{ width: "100%" }}
+                    type="text"
+                    required
+                    id="name"
+                    name="name"
+                    autoComplete="name"
+                    onChange={(e) => this.setState({ name: e.target.value })}
+                  />
+                </label>
+              </div>
 
-              <FormControl>
-                <InputLabel htmlFor="currency">Age</InputLabel>
-                <Select
-                  value={this.state.currency}
-                  onChange={e => this.setState({ name: e.target.value })}
-                  inputProps={{
-                    name: 'currency',
-                    id: 'currency',
-                  }}
-                >
-                  <MenuItem value="aud">AUD</MenuItem>
-                  <MenuItem value="gbp">GBP</MenuItem>
-                  <MenuItem value="usd">USD</MenuItem>
-                </Select>
-              </FormControl>
+              <div style={{ marginBottom: 20 }}>
+                <label>
+                  Amount
+                  <div style={{ display: "flex" }}>
+                    <select
+                      style={{ marginRight: 10 }}
+                      name="currency"
+                      id="currency"
+                      onChange={(e) =>
+                        this.setState({ currency: e.target.value })
+                      }
+                    >
+                      <option value="aud">AUD</option>
+                      <option value="gbp">GBP</option>
+                      <option value="usd">USD</option>
+                    </select>
 
-              <label>Credit Card Details</label>
-              <CardElement />
+                    <input
+                      style={{ width: "100%" }}
+                      type="number"
+                      required
+                      id="amount"
+                      name="amount"
+                      onChange={(e) =>
+                        this.setState({ amount: e.target.value })
+                      }
+                    />
+                  </div>
+                </label>
+              </div>
 
-              <button type="submit">Pay</button>
+              <div>
+                <label>Card Details</label>
+
+                <CardElement />
+              </div>
+
+              <Button
+                variant="outlined"
+                style={{ marginTop: 20 }}
+                type="submit"
+              >
+                Pay
+              </Button>
             </form>
-          </React.Fragment>
+          </div>
         )}
       </Mutation>
     );
